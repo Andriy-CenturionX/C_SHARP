4d8264f03550a0e29ed0da49d6bd6c632d4ee62d
Andriy-CenturionX <gasukandrej2@gmail.com>
2025-12-09 15:18:03 +0200

    Calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+
+        double a = 10;
+        double b = 5;
+
+        Console.WriteLine($"Add: {calc.Add(a, b)}");
+        Console.WriteLine($"Subtract: {calc.Subtract(a, b)}");
+        Console.WriteLine($"Multiply: {calc.Multiply(a, b)}");
+
+        try
+        {
+            Console.WriteLine($"Divide: {calc.Divide(a, b)}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
