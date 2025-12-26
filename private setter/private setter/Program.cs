b323c94c3c9c2cb6d2eac13cdfb79a5dc226e670
Andriy-CenturionX <gasukandrej2@gmail.com>
2025-12-26 16:53:16 +0200

    Add files via upload

---
+++ private setter/private setter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class BankAccount
+{
+    public string AccountNumber { get; }
+
+    public decimal Balance { get; private set; }
+
+    public BankAccount(string accountNumber, decimal initialBalance = 0)
+    {
+        AccountNumber = accountNumber;
+        Balance = initialBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+            Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > 0 && amount <= Balance)
+            Balance -= amount;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount("UA123456", 1000);
+
+        account.Deposit(300);
+        account.Withdraw(250);
+
+        Console.WriteLine($"Рахунок: {account.AccountNumber}");
+        Console.WriteLine($"Баланс: {account.Balance}");
+    }
+}
