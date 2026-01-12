93ba3dbcb3b67e8139495e2fd6d2020c54510c92
Andriy-CenturionX <gasukandrej2@gmail.com>
2026-01-12 17:00:54 +0200

    Add files via upload

---
+++ ICalc/ICalc/ArrayContainer.cs
@@ -0,0 +1,27 @@
+﻿class ArrayContainer : ICalc
+{
+    private int[] data;
+
+    public ArrayContainer(int[] data)
+    {
+        this.data = data;
+    }
+
+    public int Less(int valueToCompare)
+    {
+        int count = 0;
+        foreach (int x in data)
+            if (x < valueToCompare)
+                count++;
+        return count;
+    }
+
+    public int Greater(int valueToCompare)
+    {
+        int count = 0;
+        foreach (int x in data)
+            if (x > valueToCompare)
+                count++;
+        return count;
+    }
+}
---
+++ ICalc/ICalc/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 3, 5, 7, 9 };
+        ArrayContainer arr = new ArrayContainer(numbers);
+
+        Console.WriteLine("Менших за 5: " + arr.Less(5));
+        Console.WriteLine("Більших за 5: " + arr.Greater(5));
+    }
+}
