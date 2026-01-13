8e89e314e1489885450b223196ab8e4c8aeb305b
Andriy-CenturionX <gasukandrej2@gmail.com>
2026-01-13 21:18:14 +0200

    mInstrument

---
+++ MusicInstrument/MusicalInstrument/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class MusicalInstrument
+{
+    protected string Name;
+    protected string Description;
+    protected string HistoryText;
+
+    public MusicalInstrument(string name, string desc, string history)
+    {
+        Name = name;
+        Description = desc;
+        HistoryText = history;
+    }
+
+    public virtual void Sound() { }
+    public void Show() => Console.WriteLine(Name);
+    public void Desc() => Console.WriteLine(Description);
+    public void History() => Console.WriteLine(HistoryText);
+}
+
+class Violin : MusicalInstrument
+{
+    public Violin() : base("Скрипка", "Струнний інструмент", "З’явилась у XVI столітті") { }
+    public override void Sound() => Console.WriteLine("Віііі...");
+}
+
+class Program
+{
+    static void Main()
+    {
+        MusicalInstrument m = new Violin();
+        m.Show();
+        m.Sound();
+        m.Desc();
+        m.History();
+
+        Console.ReadKey();
+    }
+}
