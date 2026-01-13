8c6391526b6f202352c5ea957469e809997b5751
Andriy-CenturionX <gasukandrej2@gmail.com>
2026-01-13 21:15:56 +0200

    Worker

---
+++ Workeeer/Worker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+abstract class Worker
+{
+    public abstract void Print();
+}
+
+class President : Worker
+{
+    public override void Print() => Console.WriteLine("Президент");
+}
+
+class Security : Worker
+{
+    public override void Print() => Console.WriteLine("Охоронець");
+}
+
+class Manager : Worker
+{
+    public override void Print() => Console.WriteLine("Менеджер");
+}
+
+class Engineer : Worker
+{
+    public override void Print() => Console.WriteLine("Інженер");
+}
+
+class Program
+{
+    static void Main()
+    {
+        Worker w = new Engineer();
+        w.Print();
+
+        Console.ReadKey();
+    }
+}
