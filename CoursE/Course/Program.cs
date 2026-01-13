e4c41f160a4a2e5e3787d3cd2af639be1fa8c740
Andriy-CenturionX <gasukandrej2@gmail.com>
2026-01-13 21:22:45 +0200

    Course

---
+++ CoursE/Course/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Course
+{
+    public string Name { get; set; }
+    public int Duration { get; set; }
+
+    public Course(string name, int duration)
+    {
+        Name = name;
+        Duration = duration;
+    }
+
+    public override string ToString()
+    {
+        return $"Курс: {Name}, Тривалість: {Duration} годин";
+    }
+}
+
+class OnlineCourse : Course
+{
+    public string Platform { get; set; }
+
+    public OnlineCourse(string name, int duration, string platform)
+        : base(name, duration)
+    {
+        Platform = platform;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", Платформа: {Platform}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Course c = new Course("C#", 40);
+        OnlineCourse oc = new OnlineCourse("OOP", 30, "Udemy");
+
+        Console.WriteLine(c);
+        Console.WriteLine(oc);
+
+        Console.ReadKey();
+    }
+}
