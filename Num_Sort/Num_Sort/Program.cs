cde1312092cdb198d22dc4366249a2bbc9436460
Andriy-CenturionX <gasukandrej2@gmail.com>
2026-02-13 11:17:03 +0200

    Add files via upload

---
+++ Num_Sort/Num_Sort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static int DigitSum(int number)
+    {
+        number = Math.Abs(number);
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+
+    static void Main()
+    {
+        int[] numbers = { 121, 75, 81 };
+
+        var descending = numbers
+            .OrderByDescending(n => DigitSum(n))
+            .ToArray();
+
+        var ascending = numbers
+            .OrderBy(n => DigitSum(n))
+            .ToArray();
+
+        Console.WriteLine("За зростанням:");
+        foreach (var n in descending)
+            Console.WriteLine(n);
+
+        Console.WriteLine("За спаданням:");
+        foreach (var n in ascending)
+            Console.WriteLine(n);
+    }
+}
